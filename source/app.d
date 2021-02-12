dfcd8548f5017f0dcd81280fa1268c781c1be7b1
t. Macleod Sawyer <macleod@macleodsawyer.com>
2021-02-11 23:38:06 -0600

    Fixed indentations

--- source/app.d
+++ source/app.d
@@ -22,25 +22,24 @@ class API : APIRoot {
 void main()
 {
 	auto router = new URLRouter;
-    router.registerRestInterface(new API());
+	router.registerRestInterface(new API());
 
 	// Serve all Static Files from the /public folder
 	router
 		.get("/", &showHome)
 		.get("*", serveStaticFiles("public"));
 
-    // GET http://127.0.0.1:8080/api/ and 'Hello, World' will be replied
+	// GET http://127.0.0.1:8080/api/ and 'Hello, World' will be replied
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["127.0.0.1"];
 
-    listenHTTP(settings, router);
+	listenHTTP(settings, router);
 
 	auto svelteBuild = executeShell("cd svelte && npm run build");
 	if (svelteBuild.status == 0) {
 		writeln(svelteBuild.output);
 		writeln(C.blueDark.fg, "[Svelte Built] Listening for requests on http://", settings.bindAddresses[0], ":", settings.port, resetColor);
-
 		runApplication();
 	}
 	if (svelteBuild.status != 0) writeln("Svelte build failed:\n", svelteBuild.output);
